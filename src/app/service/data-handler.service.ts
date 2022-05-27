26b612240135b9ec4ee6415ba553cea13cf34cbb
Yevgen <kulakov.ewgen@gmail.com>
2022-05-27 21:59:26 +0200

    Добавил отображение всех задач

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import {Category} from "../model/Category";
+import {TestData} from "../data/TestData";
+import {Task} from "../model/Task";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,12 @@ import { Injectable } from '@angular/core';
 export class DataHandlerService {
 
   constructor() { }
+
+  getCategories():Category[] {
+    return TestData.categories;
+  }
+
+  getTasks():Task[]{
+    return TestData.tasks;
+  }
 }
